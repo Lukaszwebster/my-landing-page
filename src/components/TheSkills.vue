2f5dadb335c785d35ad38d60c2ac254369c96088
Łukasz Samcik <l.samcik@gmail.com>
2023-07-11 21:58:50 +0200

    adding skills component

--- src/components/TheSkills.vue
+++ src/components/TheSkills.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <h1 class="text-black">
-      aaaa
-    </h1>
+  <div class="mx-4 mt-10 font-roboto lg:mx-[80px]">
+    <h2 class="text-[1.5rem]">
+      Technologie z których korzystam
+    </h2>
+    <h2 class="mt-10 leading-loose text-gray md:ml-[30px]">
+      Od kiedy uczę się frontendu, staram się poznawać jak najwięcej nowych technologii. Zdaję sobie sprawę z tego, że tylko ciągłą nauką można nadążyć za rozwijającą się branża IT.
+      Jednocześnie wiem, że pracując już na stanowisku frontend developera będę musiał cały czas poszerzać swoją wiedzę i rozwijać się w kolejnych gałęziach technologicznych.
+    </h2>
   </div>
 </template>
