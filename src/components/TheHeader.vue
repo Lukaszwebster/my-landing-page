88a9697629c2c4c7b37b26e38b7fd1e1dc959a0a
Łukasz Samcik <l.samcik@gmail.com>
2023-07-11 11:28:17 +0200

    making header with navigation

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const { t, locale } = useI18n()
+
+// async function toggleLocales() {
+//   // change to some real logic
+//   const locales = availableLocales
+//   const newLocale = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+//   await loadLanguageAsync(newLocale)
+//   locale.value = newLocale
+// }
+</script>
+
+<template>
+  <div class="ml-8 mr-8 mt-12 flex items-center justify-between">
+    <div class="text-5">
+      Moje<span class="font-1000">Logo</span>
+    </div>
+    <nav class="flex gap-2">
+      <a href="https://github.com/Lukaszwebster">
+        <img class="h-5 w-5" src="/github.svg" alt="">
+      </a>
+
+      <a href="https://www.linkedin.com/in/%C5%82ukasz-samcik-272222271/">
+        <img class="h-5 w-5" src="/linkedin.svg" alt="">
+      </a>
+
+      <button :title="t('button.toggle_dark')" @click="toggleDark()">
+        <div i="carbon-sun dark:carbon-moon" />
+      </button>
+    </nav>
+  </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -100,8 +100,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
+        name: 'Landing page',
+        short_name: 'Landing',
         theme_color: '#ffffff',
         icons: [
           {
