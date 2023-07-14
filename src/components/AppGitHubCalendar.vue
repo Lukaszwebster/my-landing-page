9804b533271f3464ec8d429346f8f103541ffdc5
Łukasz Samcik <l.samcik@gmail.com>
2023-07-14 23:39:15 +0200

    styling AppGitHubCalendar

--- src/components/AppGitHubCalendar.vue
+++ src/components/AppGitHubCalendar.vue
@@ -8,5 +8,12 @@ onMounted(() => {
 </script>
 
 <template>
-  <div ref="calendar" />
+  <div>
+    <h2 class="mx-4 mt-10 text-[1.5rem] lg:mx-[80px]">
+      GitHub activity
+    </h2>
+    <div class="mx-4 mt-4 lg:mx-[100px] xl:mx-[300px]">
+      <div ref="calendar" />
+    </div>
+  </div>
 </template>
