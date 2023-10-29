fc7c0e3563923791e67ccc487970803c0f6fbe72
Łukasz Samcik <l.samcik@gmail.com>
2023-10-29 12:29:27 +0100

    batter styles for button in AppProject.vue

--- src/components/AppProject.vue
+++ src/components/AppProject.vue
@@ -17,15 +17,16 @@ const props = defineProps({
 
 <template>
   <div class="mx-4 mt-20 items-center font-roboto 2xl:mx-[300px] lg:mx-[80px] md:mt-30">
-    <div data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="800" class="mt-20 flex flex-col items-center justify-center md:mt-30">
+    <div data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="800" class="mt-20 flex flex-col items-center justify-center gap-2 md:mt-30">
       <h2 class="text-center text-[1.5rem]">
         {{ props.title }}
       </h2>
-      <h2 class="class= mt-4 text-center leading-loose text-gray md:max-w-800px md:text-left">
+      <h2 class="text-center leading-loose text-gray md:max-w-800px md:text-left">
         {{ props.subtitle }}
       </h2>
-      <button class="bg-gray-200">
+      <button class="button relative flex items-center gap-1 rounded-sm bg-gray-100 py-[0.2rem] pl-[1.2rem] pr-[1.4rem] transition-400 hover:bg-gray-200">
         O projekcie
+        <div class="i-ic:baseline-arrow-right-alt arrow right-[1.4rem] transition-400" />
       </button>
       <picture class="mt-10 md:max-w-800px">
         <a :href="imgUrl">  <img class="rounded-8px" :src="imgSrc" alt=""></a>
@@ -33,3 +34,10 @@ const props = defineProps({
     </div>
   </div>
 </template>
+
+<style>
+.button:hover > .arrow {
+  /* right: 0.8rem; */
+  transform: translate(0.5rem);
+  }
+</style>
