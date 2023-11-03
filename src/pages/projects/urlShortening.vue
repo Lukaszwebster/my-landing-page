24035afbf564c81b3252ff63df3d6bb65ed036b0
Łukasz Samcik <l.samcik@gmail.com>
2023-11-03 11:18:36 +0100

    added text and img to urlShortening.vue

--- src/pages/projects/urlShortening.vue
+++ src/pages/projects/urlShortening.vue
@@ -1,3 +1,7 @@
+<script setup>
+const techItems = ['html', 'css', 'js']
+</script>
+
 <template>
   <div class="mx-4 mt-12 font-roboto 2xl:mx-[300px] lg:mx-[80px]">
     <div class="max-w-[40rem]">
@@ -7,6 +11,27 @@
       <h2 class="text-gray">
         Skracanie linków z Frontend Mentor.
       </h2>
+      <p class="mt-12 text-gray">
+        Jest to jeden z pierwszych moich projektów, który wykonywałem w ramach nauki z serwisem Frontend Mentor.
+      </p>
+    </div>
+    <div class="mt-[4rem] w-full flex flex-col justify-between md:flex-row">
+      <div>
+        <h2 class="text-gray">
+          Technologie w projekcie
+        </h2>
+        <div class="mt-[2rem] flex flex-col gap-1">
+          <ul v-for="(item, index) in techItems" :key="index">
+            <li class="w-min rounded-4px bg-#fafafa px-2 py-1 hover:bg-gray dark:text-black">
+              {{ item }}
+            </li>
+          </ul>
+        </div>
+        <div class="mt-[2rem]">
+          <a class="underline" href="https://lukaszwebster.github.io/Shortly-URL/">Obejrzyj stronę</a>
+        </div>
+      </div>
+      <img class="rounded-[8px] lg:max-w-[40rem] md:max-w-[30rem] xl:max-w-[50rem]" src="/shortly.jpeg" alt="">
     </div>
   </div>
 </template>
