c6c930e3816d1b4a128a11d647cf8d1a5c44e6fa
Łukasz Samcik <l.samcik@gmail.com>
2023-11-04 19:10:04 +0100

    fixed routerLink

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ const routes = setupLayouts(generatedRoutes)
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
   App,
-  { routes, base: import.meta.env.BASE_URL },
+  { routes, base: import.meta.env.BASE_URL, scrollBehavior() { return { top: 0 } } },
   (ctx) => {
     // install all modules under `modules/`
     Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
