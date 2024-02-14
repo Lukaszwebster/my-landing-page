5f20d6903b59de4ceb6a8eca802e8fb38143f745
Łukasz Samcik <l.samcik@gmail.com>
2024-02-14 09:00:07 +0100

    added new text for project

--- src/pages/projects/wycenaai.vue
+++ src/pages/projects/wycenaai.vue
@@ -3,10 +3,10 @@ const projectOverviewItems = [
   {
     title: 'Wycena.AI',
     subtitle: 'Wycena nieruchomości',
-    description: 'Był to mój pierwszy projekt komercyjny',
+    description: 'Był to mój pierwszy projekt komercyjny, w którym pracowałem jako junior. Projekt ten składa się z dwóch części. Landing page z podstronami oraz aplikacji do wyceny nieruchomości dostępnej po zalogowaniu.',
     imgUrl: '/wycena-ai.png',
     href: 'https://wycena.ai',
-    skills: ['vue', 'nuxt'],
+    skills: ['vue', 'vite', 'nuxt', 'tailwind'],
   },
 ]
 </script>
