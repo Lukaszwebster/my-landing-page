e6be1c1d0287f55f1e1006a57683832020e1d6ec
Łukasz Samcik <l.samcik@gmail.com>
2024-02-14 09:02:21 +0100

    added new text

--- src/pages/projects/socialSeal.vue
+++ src/pages/projects/socialSeal.vue
@@ -2,7 +2,7 @@
 const projectOverviewItems = [
   {
     title: 'Social Seal landing page',
-    subtitle: 'Landing page, który wykonałem w ramach rekrutacji.',
+    subtitle: 'Landing page, który wykonałem w ramach rekrutacji dla jednej z firm. Początkowo miał być wykonany tylko w celu wysłania go do potencjalnego pracodawcy. W procesie tworzenia projektu zauważyłem jednak, że nauczył mnie wielu nowych rzeczy i postanowiłem umieścić go tutaj.',
     description: '',
     imgUrl: '/socialSeal.png',
     href: 'https://splendorous-phoenix-798e6b.netlify.app/',
