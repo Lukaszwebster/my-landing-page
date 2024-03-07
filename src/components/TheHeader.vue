53dc379112ae148e95df7e07cd5e6aec0e5ec9a9
Łukasz Samcik <l.samcik@gmail.com>
2024-03-07 10:39:19 +0100

    added text for TheHeader.vue. Added background image. Installed swiper

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="mx-4 font-roboto 2xl:mx-[300px] lg:mx-[80px]">
+    <img class="absolute top-0 opacity-40% -z-10" src="/background.svg" alt="">
     <div data-aos="fade" data-aos-offset="200" data-aos-duration="800" data-aos-mirror="true" class="mt-20">
       <div class="sm:flex sm:justify-between">
         <picture class="max-w-300px lg:max-w-400px xl:max-w-500px">
@@ -7,13 +8,13 @@
         </picture>
         <div class="lg:max-w-550px md:max-w-400px sm:max-w-500px xl:max-w-700px">
           <h1 class="mt-10 text-[1.8rem] font-light leading-tight sm:mt-0 sm:text-right lg:text-[2.5rem] lg:text-[3rem] md:text-[2.1rem]">
-            <span class="">Siema.</span> <br> <span class="font-bold"> Mam na imię Łukasz.</span> <br> <span class="">Jestem junior frontend developerem.</span>
+            <span class="">Siema.</span> <br> <span class="font-bold"> Mam na imię Łukasz.</span> <br> <span class="">Jestem frontend developerem.</span>
           </h1>
 
           <h2 class="mt-10 leading-loose text-gray-500 md:ml-[30px] sm:text-right">
             Przez 8 lat pracowałem jako instruktor i wykładowca nauki jazdy. <br> Po tym czasie podjąłem decyzje o przebranżowieniu się na stanowisko frontend developera. Naukę rozpocząłem 3 lata temu. Od początku tego procesu korzystałem z płatnych i darmowych kursów online. Z poszerzania wiedzy oraz pracy nad projektami czerpię duzo przyjemnosci i satysfakcji.
             <br>
-            Prywatnie - fan motoryzacji, miłośnik narciarstwa/kolarstwa oraz gier komputerowych.
+            Prywatnie - fan motoryzacji, pasjonat foto/video, miłośnik narciarstwa/kolarstwa oraz gier komputerowych.
           </h2>
         </div>
       </div>
